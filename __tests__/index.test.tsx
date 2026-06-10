1031356b33dcfb215ab6b2a7526f2d76cf45c37f
Trungdat35 <dotrungdat.mda@gmail.com>
2026-06-10 05:47:12 +0300

    feat

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -9,7 +9,7 @@ describe('Home', () => {
     render(<Home />)
 
     const heading = screen.getByRole('heading', {
-      name: /welcome to holetex/i,
+      name: /welcome to Dat/i,
     })
 
     expect(heading).toBeInTheDocument()
